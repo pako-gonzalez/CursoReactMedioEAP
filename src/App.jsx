fc9e3566cbd2e9da806162064c097cae2ffe4bbe
unknown <kikero25@gmail.com>
2025-02-24 10:34:32 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="content-center">
+        <div className="m-4 ">
+          <Button>Click me</Button>
+        </div>
+
+      </div>
+    </>
+
+  )
+}
+
+export default App
